b01174dd4d5f836b600fa987532c9395e67988ce
LeiWithP <pedro.barrita1029@gmail.com>
2023-04-13 02:09:24 -0600

    Menu for Mapa

--- lib/components/ubicacionSelector.dart
+++ lib/components/ubicacionSelector.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ponymapscross/components/dropdownUbication.dart';
 
 class UbicacionSelector extends StatelessWidget {
   //final VoidCallback onPressed;
@@ -12,10 +13,64 @@ class UbicacionSelector extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 300,
-      height: 150,
+      height: 120,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[200],
+        color: Theme.of(context).colorScheme.onSecondary,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: 35,
+                      right: 5
+                  ),
+                  child: DropdownUbication(),
+                ),
+                Icon(Icons.arrow_forward_rounded),
+                Text(
+                  ' . . . ',
+                  style: TextStyle(fontSize: 40.0),
+                ),
+                //Icon(Icons.arrow_forward_rounded),
+                Padding(
+                  padding: EdgeInsets.only(right: 35),
+                  child: DropdownUbication(),
+                ),
+              ],
+            )
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25,
+                  right: 10,
+                ),
+                child: MaterialButton(
+                  onPressed: (){},
+                  color: Colors.deepPurpleAccent,
+                  child: const Text(
+                    'Limpiar',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              MaterialButton(
+                onPressed: (){},
+                color: Colors.teal,
+                child: const Text(
+                  'Crear Ruta',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
