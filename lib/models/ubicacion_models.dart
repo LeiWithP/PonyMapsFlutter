d7e5978af1f80b978f7865729820454103c1c5e7
Jorge Angel Diaz <jorge.angelus0107@gmail.com>
2023-04-13 23:13:58 -0600

    Basic Routing, button implementation pending

--- lib/models/ubicacion_models.dart
+++ lib/models/ubicacion_models.dart
@@ -1,20 +1,126 @@
 
-class Album {
-  final int userId;
-  final int id;
-  final String title;
+class Routes {
+  String weightName;
+  double weight;
+  double duration;
+  double distance;
+  List<Leg> legs;
+  Geometry geometry;
 
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
+  Routes({
+    required this.weightName,
+    required this.weight,
+    required this.duration,
+    required this.distance,
+    required this.legs,
+    required this.geometry
   });
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
+  factory Routes.fromJson(Map<String, dynamic> json) {
+    var legList = json['legs'] as List<dynamic>;
+
+    List<Leg> legs = legList.map((leg) => Leg.fromJson(leg)).toList();
+
+    return Routes(
+      weightName: json['weight_name'],
+      weight: json['weight'],
+      duration: json['duration'],
+      distance: json['distance'],
+      legs: legs,
+      geometry: Geometry.fromJson(json['geometry']),
+    );
+  }
+}
+
+class Leg {
+  List<dynamic> viaWaypoints;
+  List<Admin> admins;
+  double weight;
+  double duration;
+  List<dynamic> steps;
+  double distance;
+  String summary;
+
+  Leg({
+    required this.viaWaypoints,
+    required this.admins,
+    required this.weight,
+    required this.duration,
+    required this.steps,
+    required this.distance,
+    required this.summary,
+  });
+
+  factory Leg.fromJson(Map<String, dynamic> json) {
+    var adminList = json['admins'] as List<dynamic>;
+
+    List<Admin> admins = adminList.map((admin) => Admin.fromJson(admin)).toList();
+
+    return Leg(
+      viaWaypoints: json['via_waypoints'],
+      admins: admins,
+      weight: json['weight'],
+      duration: json['duration'],
+      steps: json['steps'],
+      distance: json['distance'],
+      summary: json['summary'],
+    );
+  }
+}
+
+class Admin {
+  String iso31661Alpha3;
+  String iso31661;
+
+  Admin({
+    required this.iso31661Alpha3,
+    required this.iso31661,
+  });
+
+  factory Admin.fromJson(Map<String, dynamic> json) {
+    return Admin(
+      iso31661Alpha3: json['iso_3166_1_alpha3'],
+      iso31661: json['iso_3166_1'],
+    );
+  }
+}
+
+class Waypoint {
+  double distance;
+  String name;
+  List<double> location;
+
+  Waypoint({
+    required this.distance,
+    required this.name,
+    required this.location,
+  });
+
+  factory Waypoint.fromJson(Map<String, dynamic> json) {
+    return Waypoint(
+      distance: json['distance'],
+      name: json['name'],
+      location: List<double>.from(json['location']),
+    );
+  }
+}
+
+class Geometry {
+  final List<List<double>> coordinates;
+  final String type;
+
+  Geometry({
+    required this.coordinates,
+    required this.type
+  });
+
+  factory Geometry.fromJson(Map<String, dynamic> json) {
+    var coordinatesList = json['coordinates'] as List;
+    List<List<double>> coordinates = coordinatesList.map((coordinate) => List<double>.from(coordinate)).toList();
+
+    return Geometry(
+      coordinates: coordinates,
+      type: json['type'],
     );
   }
 }
--- lib/screens/mapa.dart
+++ lib/screens/mapa.dart
@@ -15,6 +15,8 @@ import '../models/map_marker_model.dart';
 
 import 'package:http/http.dart' as http;
 
+import '../models/ubicacion_models.dart';
+
 class Mapa extends StatefulWidget {
   const Mapa({Key? key}) : super(key: key);
 
@@ -31,9 +33,6 @@ class _MapaState extends State<Mapa> {
     polylines: [],
   );
   List<LatLng> polylinePoints = [];
-
-  late PolylineLayer polyLineLayer;
-  late Polyline currentPolyline;
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +69,8 @@ class _MapaState extends State<Mapa> {
             TileLayer(
               urlTemplate:
               "https://api.mapbox.com/styles/v1/angels0107/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
+              /*urlTemplate:
+              "https://tile.openstreetmap.org/{z}/{x}/{y}.png",*/
               additionalOptions: const {
                 'mapStyleId': AppKeys.mapBoxStyleId,
                 'accessToken': AppKeys.mapBoxAccessToken,
@@ -126,8 +127,13 @@ class _MapaState extends State<Mapa> {
                             content: Text('Normal Tap'),
                           ));*/
 
-                          final apiData = await fetchApiData();
-                          print(apiData.data);
+
+                            await addCoordinates(LatLng(19.722989, -101.185827), LatLng(19.723173, -101.184928));
+                            print(polylinePoints.join(''));
+                           //polylinePoints.add(LatLng(19.72299, -101.18582));
+                           //polylinePoints.add(LatLng(19.72325, -101.18541));
+                            addPolyline();
+                          //fetchData();
                         },
                         onLongPress: () {
                           ScaffoldMessenger.of(_).showSnackBar(const SnackBar(
@@ -170,6 +176,23 @@ class _MapaState extends State<Mapa> {
       ],
     );
   }
+  ///
+  /// Routing
+  ///
+
+  Future<void> addCoordinates(LatLng origen, LatLng destino) async {
+
+    final apiData = await fetchApiData(origen, destino);
+    var coordinates = apiData.routes.first.geometry.coordinates;
+
+    polylinePoints = coordinates.map(
+            (coordinate) =>
+                LatLng( coordinate.last, coordinate.first)
+    ).toList();
+
+
+
+  }
 
   void addPolyline() {
     setState(() {
@@ -187,30 +210,86 @@ class _MapaState extends State<Mapa> {
     });
   }
 
-  Future<ApiResponse> fetchApiData() async {
+  Future<ApiResponse> fetchApiData(LatLng origen,LatLng destino) async {
+
+    var accessToken = AppKeys.mapBoxAccessToken;
+
+    var ori_lng =  origen.longitude;
+    var ori_lat =  origen.latitude;
+
+    var dest_lng =  destino.longitude;
+    var dest_lat =  destino.latitude;
+
+    String urlTemplate = "https://api.mapbox.com/directions/v5/mapbox/walking/$ori_lng,$ori_lat;$dest_lng,$dest_lat?alternatives=false&geometries=geojson&overview=simplified&steps=false&access_token=$accessToken";
+
     final apiUrl = Uri.parse(
-        'https://api.mapbox.com/directions/v5/mapbox/walking/-101.185404%2C19.723222%3B-101.185026%2C19.723415?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token=pk.eyJ1IjoiYW5nZWxzMDEwNyIsImEiOiJjbGJ2anRvdXAwdTMwM3ZxbzFkeWJndThqIn0.Ep3N8cDHH3Iwr9YLDgQn8g');
+        //'https://api.mapbox.com/directions/v5/mapbox/walking/-101.185404%2C19.723222%3B-101.185026%2C19.723415?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token=pk.eyJ1IjoiYW5nZWxzMDEwNyIsImEiOiJjbGJ2anRvdXAwdTMwM3ZxbzFkeWJndThqIn0.Ep3N8cDHH3Iwr9YLDgQn8g');
+      urlTemplate);
     final response = await http.get(apiUrl);
 
+    print("Get data");
     if (response.statusCode == 200) {
       // API call was successful
       final jsonResponse = json.decode(response.body);
       return ApiResponse.fromJson(jsonResponse);
     } else {
       // API call failed
+
       throw Exception('Failed to fetch data from API');
     }
   }
 }
+/*
+  Future<void> fetchData() async {
+    final url = Uri.parse(''
+       // 'https://dummyjson.com/products/1'
+        'https://api.mapbox.com/directions/v5/mapbox/walking/-101.185404%2C19.723222%3B-101.185026%2C19.723415?alternatives=false&continue_straight=true&geometries=geojson&overview=simplified&steps=false&access_token=pk.eyJ1IjoiYW5nZWxzMDEwNyIsImEiOiJjbGJ2anRvdXAwdTMwM3ZxbzFkeWJndThqIn0.Ep3N8cDHH3Iwr9YLDgQn8g'
+    );
+
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // The request was successful, parse the JSON response
+      final jsonString = response.body;
+      print(jsonString);
+      // Do something with the JSON data
+    } else {
+      // The request failed with an error code, handle the error
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+*/
+
+
+
 
 class ApiResponse {
-  final String data;
+  List<Routes> routes;
+  List<Waypoint> waypoints;
+  String code;
+  String uuid;
 
-  ApiResponse({required this.data});
+  ApiResponse({
+    required this.routes,
+    required this.waypoints,
+    required this.code,
+    required this.uuid,
+  });
 
   factory ApiResponse.fromJson(Map<String, dynamic> json) {
+    var routeList = json['routes'] as List<dynamic>;
+    var waypointList = json['waypoints'] as List<dynamic>;
+
+    List<Routes> routes = routeList.map((route) => Routes.fromJson(route)).toList();
+    List<Waypoint> waypoints =
+    waypointList.map((waypoint) => Waypoint.fromJson(waypoint)).toList();
+
     return ApiResponse(
-      data: json[0],
+      routes: routes,
+      waypoints: waypoints,
+      code: json['code'],
+      uuid: json['uuid'],
     );
   }
 }
